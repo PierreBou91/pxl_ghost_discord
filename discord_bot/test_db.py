ca16c64bafd6c7591f1c8c40b6b3d34d0b00885d
Pierre Bouriat <pierre.bouriat@outlook.fr>
2022-02-09 23:38:25 +0100

    tests: testing merge client and bot

--- discord_bot/test_db.py
+++ discord_bot/test_db.py
@@ -24,6 +24,15 @@ async def on_message(message):
     if message.content == 'eltesto':
         await message.channel.send('ONLY ONE DYNO')
 
+    if message.content == 'ping':
+        await message.channel.send('pong')
+    elif message.content == 'pong':
+        await message.channel.send('NO, YOU have to say ping and ONLY I get to say pong !')
+    elif message.content == 'f':
+        await message.channel.send('f')
+    elif message.content == 'F':
+        await message.channel.send('F')
+
 @bot.command()
 async def add_user(ctx, wallet):
     db.add_user(ctx, wallet)
