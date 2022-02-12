4f07327f2e231f80172a1a17ae842940304e337a
Pierre Bouriat <pierre.bouriat@outlook.fr>
2022-02-12 16:16:56 +0100

    feat: launching and deleting giveaway done

--- discord_bot/tests.py
+++ discord_bot/tests.py
@@ -28,22 +28,6 @@ async def on_ready():
 #########################################
 
 @bot.command()
-@commands.has_any_role("Admin")
-async def giveaway(ctx, is_owner=None):
-    is_owner = True if (is_owner == "owner") else False
-    is_only_giveaway = True if db.check_only_giveaway(is_owner) else False
-    if is_only_giveaway:
-        db.launch_giveaway(ctx, is_owner)
-        await ctx.reply(texts.giveaway(is_owner))
-    else:
-        await ctx.reply(texts.already_giveaway(is_owner))
-
-@bot.command()
-@commands.has_any_role("Admin")
-async def delete(ctx, giveaway_id):
-    db.delete_giveaway(giveaway_id)
-
-@bot.command()
 async def giv(ctx, wallet):
     db.add_user(ctx, wallet)
     db.safe_add_to_giveaway(ctx)
