230fe179818b616ab597fe793c0e0701ceb886a5
Pierre Bouriat <pierre.bouriat@outlook.fr>
2022-02-19 18:10:36 +0100

    fix: testing

--- discord_bot/tests.py
+++ discord_bot/tests.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from os import environ
 import texts
 import db_discord_helper as db
+import re
 
 #########################################
 #           BOILER PLATE CODE           #
@@ -31,6 +32,14 @@ async def on_message(message):
     # don't respond to ourselves
     if message.author.id == 931674481628434503:
         return
+
+    if message.channel.id == 931679915248594944:
+        if db.check_if_ongoing_giveaway():
+            if not bool(re.match(r"^0x[a-fA-F0-9]{40}$", message.content)):
+                await message.reply(texts.channel_reserved())
+                await message.reply(texts.wrong_wallet_address())
+        else:
+            await message.channel.send(texts.no_ongoing_giveway())
 
     # necessary call for the commands to work
     await bot.process_commands(message)
